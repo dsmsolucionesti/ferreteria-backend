08b7770a995510a2335650c450f52cbb955b4641
Diego Patricio Muñoz Mauro <d.munozmauro@hotmail.com>
2026-03-21 02:33:17 -0300

    swagger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import "reflect-metadata";
-import { createExpressServer } from "routing-controllers";
+import {
+  createExpressServer,
+  getMetadataArgsStorage,
+} from "routing-controllers";
+import { routingControllersToSpec } from "routing-controllers-openapi";
+import swaggerUi from "swagger-ui-express";
 
 import { ActiveSysController } from "./features/active-sys/active-sys.routes";
 import { CategoriaController } from "./features/categoria/controller/categoria.controller";
@@ -8,7 +13,29 @@ import { ProductoController } from "./features/producto/controller/producto.cont
 
 const app = createExpressServer({
   routePrefix: "/api",
-  controllers: [ActiveSysController, CategoriaController, UsuarioController, ProductoController],
+  controllers: [
+    ActiveSysController,
+    CategoriaController,
+    UsuarioController,
+    ProductoController,
+  ],
 });
+
+const storage = getMetadataArgsStorage();
+
+const spec = routingControllersToSpec(
+  storage,
+  {},
+  {
+    info: {
+      title: "API Ferretería",
+      version: "1.0.0",
+      description: "Documentación de la API",
+    },
+  },
+);
+
+// 🔥 SWAGGER UI
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(spec));
 
 export default app;
