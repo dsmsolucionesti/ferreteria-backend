4cd44d066b526efb7818a0831b16662d10433dd6
Diego Patricio Muñoz Mauro <d.munozmauro@hotmail.com>
2026-03-27 14:42:44 -0300

    upd delete cascade

--- query-delete-cascade.sql
+++ query-delete-cascade.sql
@@ -1,6 +1,9 @@
-delete from clientes;
-delete from usuarios;
 delete from movimientos_stock;
 delete from producto_precios;
+delete from cotizacion_detalle;
 delete from productos;
-delete from categorias;
+delete from cotizaciones;
+delete from clientes;
+delete from categorias; 
+
+-- delete from usuarios;
