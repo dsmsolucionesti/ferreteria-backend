c0db3ff2dff51c540f01ec7759e85cbfdc8756f7
Diego Patricio Muñoz Mauro <d.munozmauro@hotmail.com>
2026-03-29 20:29:30 -0300

    Editar cotizacion

--- src/features/cotizacion/interfaces/cotizacion.repository.interface.ts
+++ src/features/cotizacion/interfaces/cotizacion.repository.interface.ts
@@ -7,6 +7,6 @@ export interface CotizacionRepositoryInterface {
   findAll(client?: PoolClient): Promise<RespuestaProceso<Cotizacion[]>>;
   findById(id: number, client?: PoolClient): Promise<RespuestaProceso<Cotizacion>>;
   post(data: Partial<CotizacionRequest>, client?: PoolClient): Promise<RespuestaProceso>;
-  update(id: number, data: Partial<Cotizacion>, client?: PoolClient): Promise<RespuestaProceso>;
+  update(id: number, data: any, client?: PoolClient): Promise<RespuestaProceso>;
   delete(id: number, client?: PoolClient): Promise<RespuestaProceso>;
 }
